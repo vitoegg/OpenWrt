f3bc64e1f7278e4d871da89114ec9646420000f6
PanLiu <vitoegg@gmail.com>
2021-06-15 19:45:28 +0800

    Create customize-sta.sh

---
+++ customize-sta.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: customize-sta.sh
+# Description: OpenWrt DIY script (After Update feeds)
+#
+
+# 修改内核版本为4.19
+sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# 取消系统默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+
+# Add OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash
+
+# 原版Dockerman
+#rm -rf ../lean/luci-app-docker
+#git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+#git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+
+popd
+
+#openclash依赖
+rm -rf feeds/packages/libs/libcap
+svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
