3df14015c833b734cc79e36d313ade4caf256fb6
PanLiu <vitoegg@gmail.com>
2021-06-16 09:48:35 +0800

    Update sta.sh

--- sta.sh
+++ sta.sh
@@ -11,7 +11,8 @@
 #
 
 # 修改内核版本为4.19
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
