8513694cfb38195c06f0d77b7f6a557e20120a3c
PanLiu <vitoegg@gmail.com>
2021-06-17 14:13:14 +0800

    Update customize.sh

--- immortalwrt/customize.sh
+++ immortalwrt/customize.sh
@@ -1,1 +1,43 @@
+#!/bin/bash
 #
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: customize.sh
+# Description: OpenWrt DIY script (After Update feeds)
+#
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# 取消系统默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+
+# Add OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash
+
+# 原版Dockerman
+rm -rf ../lean/luci-app-docker
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
+
+popd
+
+# 修改默认主题为argon
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
