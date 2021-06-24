a94ef271afa0b6582140233e4458c40bcd4c76ab
PanLiu <vitoegg@gmail.com>
2021-06-24 15:04:11 +0800

    修改默认配置

--- Lean/customize.sh
+++ Lean/customize.sh
@@ -14,14 +14,7 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # 取消系统默认密码
-#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-
-# 增加自己个性名称
-#sed -i "s/OpenWrt /iHome $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-#echo '修改时区'
-#sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
 # Clone community packages to package/community
 mkdir package/community
@@ -43,8 +36,8 @@ rm -rf ../lean/luci-theme-argon
 
 popd
 
+# 增加日期显示
 pushd package/lean/default-settings/files
-sed -i "/CYXluq4wUazHjmCDBCqXF/d"
 export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
 popd
