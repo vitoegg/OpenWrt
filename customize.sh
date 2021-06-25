68375ad04a178bacdf3476a1355cdfc8822dca4c
PanLiu <vitoegg@gmail.com>
2021-06-25 18:45:56 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,4 @@
+
+
+# Remove some default packages
+sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
