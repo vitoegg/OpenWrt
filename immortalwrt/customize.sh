1337e58a119ecd7de70f267ad29198ac34a948b6
PanLiu <vitoegg@gmail.com>
2021-06-25 21:38:17 +0800

    修改编译的默认配置

--- immortalwrt/customize.sh
+++ immortalwrt/customize.sh
@@ -10,17 +10,11 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-default-settings
 
+#修改默认名称
+sed -i '/openwrt_luci/d' package/emortal/default-settings/files/zzz-default-settings
 
-# Clone community packages to package/community
-mkdir package/community
-pushd package/community
-
-# Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../lean/luci-theme-argon
-
-popd
+#修改主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # Remove some default packages
 sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
