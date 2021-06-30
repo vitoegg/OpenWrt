08ebdea33a6fabbb5ae09be04ed8dc4c24eedb49
PanLiu <vitoegg@gmail.com>
2021-06-30 20:34:03 +0800

    移除argon主题

--- Lean/5.4/customize.sh
+++ Lean/5.4/customize.sh
@@ -33,11 +33,6 @@ rm -rf ../lean/luci-app-docker
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
-# Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../lean/luci-theme-argon
-
 popd
 
 # 增加日期显示
