4499b1c5f7ce1b348282da92c459833f4c042e82
PanLiu <vitoegg@gmail.com>
2021-07-01 17:03:31 +0800

    Update customize.sh

--- immortalwrt/customize.sh
+++ immortalwrt/customize.sh
@@ -13,9 +13,6 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-def
 #修改主机名
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
-#修改固件信息
-echo "iHome Compiled in $(TZ=UTC-8 date "+%Y.%m.%d")" > package/base-files/files/etc/openwrt_gxqm 
-
 # 移除默认编译的插件
 sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
 
