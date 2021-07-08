92ea31ea318341ccf4f5f6966930e5ecf454f0f8
PanLiu <vitoegg@gmail.com>
2021-07-08 20:28:59 +0800

    Update customize.sh

--- immortalwrt/customize.sh
+++ immortalwrt/customize.sh
@@ -10,11 +10,6 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-default-settings
 
-#修改主机名
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-#sed -i "s/OpenWrt /iHome Build in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
-sed -i 's/.*vermagic.*/\tdate +%Y%m%d > .\/vermagic/g' include/kernel-defaults.mk
-
 
 # 移除默认编译的插件
 sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
