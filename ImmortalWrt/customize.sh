628548e4d4c2dbfb3a49fb65ad1cd1c21bbed3b6
PanLiu <vitoegg@gmail.com>
2021-08-10 09:38:54 +0800

    Update customize.sh

--- ImmortalWrt/customize.sh
+++ ImmortalWrt/customize.sh
@@ -10,13 +10,6 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-default-settings
 
-
-# 替换Dockerman为最新版
-pushd feeds/luci/applications
-rm -rf luci-app-dockerman
-svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
-popd
-
 #修改主机名
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
