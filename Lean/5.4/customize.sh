727f447953cdc7cb9307f68e4cbae8403fe9e4af
PanLiu <vitoegg@gmail.com>
2021-08-11 10:34:14 +0800

    Update customize.sh

--- Lean/5.4/customize.sh
+++ Lean/5.4/customize.sh
@@ -31,6 +31,9 @@ rm -rf ../lean/luci-app-docker
 git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
+#SmartDNS
+#git clone -b lede --depth=1 https://github.com/pymumu/luci-app-smartdns
+
 popd
 
 # 增加日期显示
