fdee10eb18877d2a37772ed2288929a68b5323e6
PanLiu <vitoegg@gmail.com>
2021-08-19 08:54:08 +0800

    Update customize.sh

--- ImmortalWrt/customize.sh
+++ ImmortalWrt/customize.sh
@@ -13,5 +13,11 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-def
 #修改主机名
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
+# 使用OpenClash源码编译
+pushd feeds/luci/applications
+rm -rf luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+popd
+
 # 移除默认编译的插件
 sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
