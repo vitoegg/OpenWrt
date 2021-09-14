a1993590c2e49f3c93fb78aaaa579ac5217fb30b
PanLiu <vitoegg@gmail.com>
2021-09-14 09:10:51 +0800

    Update customize.sh

--- Lean/5.4/customize.sh
+++ Lean/5.4/customize.sh
@@ -17,6 +17,8 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# 关闭IPv6 分配长度
+sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
 
 # Clone community packages to package/community
 mkdir package/community
@@ -25,16 +27,9 @@ pushd package/community
 # Add OpenClash
 #git clone --depth=1 https://github.com/vernesong/OpenClash
 
-# Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-
 #SmartDNS
 git clone -b lede --depth=1 https://github.com/pymumu/luci-app-smartdns
 
-# Dockerman
-#rm -rf ../lean/luci-app-docker
-#git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-#git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 popd
 
