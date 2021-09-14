c445e59c7a34dcf500052a46b20599fa804982e6
PanLiu <vitoegg@gmail.com>
2021-09-14 09:10:03 +0800

    Update customize.sh

--- Lean/5.10/customize.sh
+++ Lean/5.10/customize.sh
@@ -13,6 +13,8 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# 关闭IPv6 分配长度
+sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
 
 # 修改内核版本
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
