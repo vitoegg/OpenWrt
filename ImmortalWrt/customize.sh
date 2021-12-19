a6bdaa0b603ae79ec90f9b57106be863fd16aa14
PanLiu <vitoegg@gmail.com>
2021-12-19 17:57:36 +0800

    Update customize.sh

--- ImmortalWrt/customize.sh
+++ ImmortalWrt/customize.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.2/g' package/base-files/files/bin/config_generate
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/emortal/default-settings/files/zzz-default-settings
 
@@ -16,11 +16,6 @@ sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 # 关闭IPv6 分配长度
 sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
 
-# 使用OpenClash源码编译
-#pushd feeds/luci/applications
-#rm -rf luci-app-openclash
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-#popd
 
 # 移除默认编译的插件
-sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
+sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
