19f9ef20f84776857b536317adfc26c8e50ffd90
PanLiu <vitoegg@gmail.com>
2021-12-19 14:53:51 +0800

    Update customize.sh

--- Lean/5.10/customize.sh
+++ Lean/5.10/customize.sh
@@ -24,9 +24,9 @@ mkdir package/community
 pushd package/community
 
 # Dockerman
-rm -rf ../lean/luci-app-docker
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+#rm -rf ../lean/luci-app-docker
+#git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+#git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
 # Add OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash
