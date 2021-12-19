baf9771e6459353d2d65ebdb9182c28b377c698f
PanLiu <vitoegg@gmail.com>
2021-12-19 15:30:56 +0800

    Update customize.sh

--- Lean/5.4/customize.sh
+++ Lean/5.4/customize.sh
@@ -6,23 +6,19 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: customize-sta.sh
+# File name: customize.sh
 # Description: OpenWrt DIY script (After Update feeds)
-#
-
-# 修改内核版本
-#sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.2/g' package/base-files/files/bin/config_generate
 # 取消系统默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 # 关闭IPv6 分配长度
 sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
 
 # Clone community packages to package/community
-mkdir package/community
-pushd package/community
+#mkdir package/community
+#pushd package/community
 
 # Dockerman
 #rm -rf ../lean/luci-app-docker
@@ -32,10 +28,10 @@ pushd package/community
 # Add OpenClash
 #git clone --depth=1 https://github.com/vernesong/OpenClash
 
-popd
+#popd
 
 # 增加日期显示
-pushd package/lean/default-settings/files
-export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
-sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
-popd
+#pushd package/lean/default-settings/files
+#export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+#sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+#popd
