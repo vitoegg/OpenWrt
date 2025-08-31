89ac16ab515f747bdee0f726fb0f8c58dd0af518
vitoegg <vitoegg@gmail.com>
2025-08-31 21:34:41 +0800

    Enable auto mount

--- scripts/Server/06-custom.sh
+++ scripts/Server/06-custom.sh
@@ -17,7 +17,6 @@ log "Adding customized argon theme"
 git clone --depth=1 https://github.com/vitoegg/Argon package/app/luci-theme-argon
 mv package/app/luci-theme-argon package/new/extd/luci-theme-argon
 
-
 # Modify Hostname
 log "Modifying hostname to HomeCloud"
 sed -i 's#OpenWrt#HomeCloud#g' package/base-files/files/bin/config_generate
@@ -58,16 +57,19 @@ BEGIN { skip=0; brace_count=0; }
 }
 ' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json > /tmp/temp.json && mv /tmp/temp.json feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json
 
-# Setting up default password
-log "Setting up default password"
+# Setting up etc config
+log "Setting up etc config"
 ZZZ="package/new/default-settings/default/zzz-default-settings"
 cat >> $ZZZ <<-EOF
+# Set customizedpassword
 sed -i 's|root:::0:99999:7:::|root:$ROOT_PASSWORD_HASH:20211:0:99999:7:::|g' /etc/shadow
+# Enable auto mount
+uci set fstab.@global[0].anon_mount='1'
+uci commit fstab
 EOF
 sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
 
 # Move configuration files
-log "Creating tmp directory"
 REPO_TEMP_DIR=$(mktemp -d)
 log "Downloading pre-configuration files"
 curl -s -S -f -L -u "$REPO_USERNAME:$REPO_TOKEN" "$REPO_URL" -o "$REPO_TEMP_DIR/repo.zip" 2>/dev/null
