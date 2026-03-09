4c84db067689f5ba94b5dae9eded4f0126903118
vitoegg <vitoegg@gmail.com>
2026-03-09 09:18:49 +0800

    Simplify Router Compilation Configuration

--- Router/Scripts/Packages.sh
+++ Router/Scripts/Packages.sh
@@ -64,7 +64,7 @@ CLONE_PACKAGE "vitoegg/Argon" "main"
 
 # Nikki
 REMOVE_FEEDS "nikki"
-CLONE_PACKAGE "vitoegg/OpenNikki" "master"
+CLONE_PACKAGE "nikkinikki-org/OpenWrt-nikki" "main"
 
 # MosDNS - use upstream version (backend + luci + v2dat)
 REMOVE_FEEDS "mosdns" "v2dat"
